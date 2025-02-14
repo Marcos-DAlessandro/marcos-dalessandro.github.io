9054f3f97f63065af976fe0157d9f3f55c6a85fe
Marcos Dalessandro <marcosdalessandro1@gmail.com>
2025-02-14 11:52:22 -0300

    fix responsiveness

--- JS/progress-fill.js
+++ JS/progress-fill.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const progressBars = document.querySelectorAll(".progress-fill");
 
-    const progressValues = [10, 80]; 
+    const progressValues = [70, 10]; 
 
     progressBars.forEach((bar, index) => {
         bar.style.width = `${progressValues[index]}%`;
