cfd1a410f5421221c9b426c036d0152608d292bd
Marcos Dalessandro <marcosdalessandro1@gmail.com>
2025-02-14 20:34:37 -0300

    feat more progress bar

--- JS/progress-fill.js
+++ JS/progress-fill.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const progressBars = document.querySelectorAll(".progress-fill");
 
-    const progressValues = [70, 10]; 
+    const progressValues = [70, 10, 35, 90, 10, 100]; 
 
     progressBars.forEach((bar, index) => {
         bar.style.width = `${progressValues[index]}%`;
