280c7eeb9448cd928aec649c9868a3df525fa4bf
Marcos Dalessandro <marcosdalessandro1@gmail.com>
2025-02-15 10:38:19 -0300

    feat finish stock section

--- JS/progress-fill.js
+++ JS/progress-fill.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const progressBars = document.querySelectorAll(".progress-fill");
 
-    const progressValues = [70, 10, 35, 90, 10, 100]; 
+    const progressValues = [70, 10, 35, 90, 10, 100,20]; 
 
     progressBars.forEach((bar, index) => {
         bar.style.width = `${progressValues[index]}%`;
